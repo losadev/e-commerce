edfc02294e963a83f4c0cdc962c09011019a0533
losadev <losadauresdev@gmail.com>
2025-03-21 09:25:23 +0100

    Nombre de proyecto cambiado

---
+++ e-commerce-frontend/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="flex justify-between border-t-1 border-gray-300 py-8 px-6 gap-6 md:px-44">
+      <p className="text-gray-500">&copy; 2025 ShopEasy. All rights reserved.</p>
+      <ul className="flex gap-2 text-[1rem] font-medium">
+        <li>Terms</li>
+        <li>Privacy</li>
+        <li>Contact</li>
+      </ul>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ e-commerce-frontend/src/components/Form.tsx
@@ -0,0 +1,14 @@
+const Form = () => {
+  return (
+    <section className="py-30 text-center flex flex-col items-center">
+      <h1 className="text-5xl font-extrabold text-center">Join Our Newsletter</h1>
+      <p className="text-center mt-2 text-xl text-gray-500 font-medium">Subscribe to get special offers, free giveaways, and once-in-a-lifetime deals.</p>
+      <form className=" flex m-auto text-center mt-8" action="#" method="post">
+        <input className="border-gray-300 rounded-md border-1 px-2 py-1 placeholder:font-medium" type="text" placeholder="Enter your email"/>
+        <button className="hover:cursor-pointer bg-black text-white font-bold px-4 py-2 rounded-lg ml-2" type="submit">Subscribe</button>
+      </form>
+    </section>
+  )
+}
+
+export default Form
